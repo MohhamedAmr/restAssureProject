633b0683900a14a67173e74affc2ee4ed5bd6548
Mody <muhamed.elsarky@gmail.com>
2023-01-31 22:50:18 +0200

    One Tap Create Store

---
+++ src/test/java/pojo/DeserlizeElements.java
@@ -0,0 +1,17 @@
+package pojo;
+
+public class DeserlizeElements {
+    private String accessToken;
+
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+
+
+}
---
+++ src/test/java/steps/GetJson.java
@@ -0,0 +1,24 @@
+package data;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class GetJson {
+
+
+    public static JSONObject readAsJASONObject(String fileName) throws IOException, ParseException {
+
+        String filePath = System.getProperty("user.dir") + "/src/test/resources/" + fileName;
+        File srcFile = new File(filePath);
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jarray = (JSONObject) jsonParser.parse(new FileReader(srcFile));
+        return jarray;
+
+    }
+}
+
